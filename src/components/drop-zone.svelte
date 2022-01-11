8c7f8cb315e87208dd7cf43d5ceba7e447acde1e
bass-dandy <ctdinh93@gmail.com>
2022-01-11 05:03:59 -0500

    init

---
+++ src/components/drop-zone.svelte
@@ -0,0 +1,108 @@
+<script>
+	import {fly} from 'svelte/transition';
+	import {deserialize} from 'dbpf-transform';
+
+	export let file = null;
+
+	let fileInput; // hidden file input ref
+	let stagedFile;
+</script>
+
+<div
+	class="container"
+	transition:fly={{ y: -200, duration: 500 }}
+>
+	<div
+		class="drop-zone"
+		on:dragover|preventDefault
+		on:drop|preventDefault={async (e) => {
+			stagedFile = await e
+				.dataTransfer
+				.items[0]
+				.getAsFile();
+		}}
+	>
+		<input
+			type="file"
+			accept=".package"
+			bind:this={fileInput}
+			on:change={() => stagedFile = fileInput.files?.[0]}
+		/>
+		{#if stagedFile}
+			{stagedFile.name}
+		{:else}
+			Drag .package file here or <a on:click={fileInput.click()}>browse files</a>
+		{/if}
+	</div>
+	<div class="controls" class:hidden={!stagedFile}>
+		<button
+			aria-label="clear file selection"
+			on:click={() => {
+				stagedFile = null;
+				fileInput.value = null;
+			}}
+		>
+			✗
+		</button>
+		<button
+			aria-label="confirm file selection"
+			on:click={async () => {
+				file = deserialize(
+					await stagedFile.arrayBuffer()
+				);
+			}}
+		>
+			✓
+		</button>
+	</div>
+</div>
+
+<style>
+	input[type="file"] {
+		display: none;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 400px;
+		padding: 15px;
+		background-color: var(--color-bg);
+		border: 2px solid var(--color-accent);
+		border-radius: 15px;
+		box-shadow: 5px 5px 10px 0px var(--color-accent);
+		font-size: 1.2rem;
+	}
+
+	.drop-zone {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 200px;
+		background-color: var(--color-fg);
+		border: 2px dashed var(--color-accent);
+		border-radius: 15px;
+	}
+
+	.hidden {
+		visibility: hidden;
+	}
+
+	.controls {
+		display: flex;
+		margin-top: 15px;
+	}
+
+	.controls > button {
+		margin: 0 5px;
+		display: inline-flex;
+		align-items: center;
+		padding: 5px 15px;
+		border: 2px solid var(--color-accent);
+		border-radius: 5px;
+		background-color: var(--color-btn);
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+	import DropZone from '../components/drop-zone.svelte';
+	import '../global.css';
+	
+	let file;
+
+	$: console.log(file);
+</script>
+
+<h1>Sims 2 DBPF Package Editor</h1>
+{#if !file}
+<div class="modal">
+	<DropZone bind:file />
+</div>
+{/if}
+
+<style>
+	h1 {
+		font-size: 3rem;
+		font-family: "sims-llhp";
+		-webkit-text-stroke: 1px var(--color-accent);
+		-webkit-text-fill-color: white;
+	}
+
+	.modal {
+		position: fixed;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
