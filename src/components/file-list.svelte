1975e24e8c2ea3595e59b7abb5410e0e9fd1795e
bass-dandy <ctdinh93@gmail.com>
2022-02-12 19:06:52 -0500

    add resource search

--- src/components/file-list.svelte
+++ src/components/file-list.svelte
@@ -4,54 +4,96 @@
 	import {defaultFileName} from '../consts';
 	import {packages, activePackage} from '../stores';
 	import {formatHex} from '../util';
+
+	let search = '';
+	let resources;
+
+	const getFileName = (resource) =>
+		resource.content.filename || defaultFileName[
+			getFileType(resource.meta.typeId)
+		];
+
+	$: {
+		const regex = new RegExp(search, 'i');
+
+		resources = search
+			? Object.entries($activePackage?.resources ?? {})
+				.filter(([, resource]) => {
+					return formatHex(resource.meta.groupId, 8).match(regex)
+						|| formatHex(resource.meta.instanceId, 8).match(regex)
+						|| formatHex(resource.meta.instanceId2, 8).match(regex)
+						|| getFileName(resource).toUpperCase().match(regex)
+						|| getFileType(resource.meta.typeId).match(regex);
+				})
+			: Object.entries($activePackage?.resources ?? {});
+	}
 </script>
 
-<ul>
-{#each Object.entries($activePackage?.resources ?? {}) as [resourceId, resource] (resourceId)}
-	<li>
-		<Button
-			variant="block"
-			onClick={() => packages.openResource(resourceId)}
-			style="padding: 5px 15px;"
-		>
-			<div class="file-info">
-				<div class="filename">
-					{
-						resource.content.filename || defaultFileName[
-							getFileType(resource.meta.typeId)
-						]
-					}
+<div class="file-list">
+	<label>
+		Search
+		<input
+			type="text"
+			placeholder="Search by file name, file type, group ID, or instance ID"
+			bind:value={search}
+		/>
+	</label>
+	<ul>
+	{#each resources as [resourceId, resource] (resourceId)}
+		<li>
+			<Button
+				variant="block"
+				onClick={() => packages.openResource(resourceId)}
+				style="padding: 5px 15px;"
+			>
+				<div class="file-info">
+					<div class="filename">
+						{getFileName(resource)}
+					</div>
+					<div class="type">
+						{getFileType(resource.meta.typeId)}
+					</div>
 				</div>
-				<div class="type">
-					{getFileType(resource.meta.typeId)}
-				</div>
-			</div>
-			<dl>
-				<div class="file-meta">
-					<dt>Group</dt>
-					<dd>{formatHex(resource.meta.groupId)}</dd>
-				</div>
-				<div class="file-meta">
-					<dt>Instance (High)</dt>
-					<dd>{formatHex(resource.meta.instanceId2, 8)}</dd>
-				</div>
-				<div class="file-meta">
-					<dt>Instance</dt>
-					<dd>{formatHex(resource.meta.instanceId, 8)}</dd>
-				</div>
-			</dl>
-		</Button>
-	</li>
-{/each}
-</ul>
+				<dl>
+					<div class="file-meta">
+						<dt>Group</dt>
+						<dd>{formatHex(resource.meta.groupId)}</dd>
+					</div>
+					<div class="file-meta">
+						<dt>Instance (High)</dt>
+						<dd>{formatHex(resource.meta.instanceId2, 8)}</dd>
+					</div>
+					<div class="file-meta">
+						<dt>Instance</dt>
+						<dd>{formatHex(resource.meta.instanceId, 8)}</dd>
+					</div>
+				</dl>
+			</Button>
+		</li>
+	{/each}
+	</ul>
+</div>
 
 <style>
-	ul {
+	.file-list {
+		display: flex;
+		flex-direction: column;
 		height: 100%;
-		list-style: none;
+		overflow: hidden;
+	}
+	label {
+		margin: 10px 15px;
+	}
+	input {
+		display: block;
+		width: 100%;
+	}
+	ul {
+		flex: 1;
 		margin: 0;
 		padding: 5px 15px;
 		overflow: auto;
+		list-style: none;
 	}
 	li {
 		width: 100%;
