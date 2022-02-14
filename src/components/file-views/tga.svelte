c0ffa9ab842c7b011e4a0b64074a1139d147a915
bass-dandy <ctdinh93@gmail.com>
2022-02-14 03:57:22 -0500

    properly render tga files

--- src/components/file-views/tga.svelte
+++ src/components/file-views/tga.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
+	import tgaImage from 'tgaimage';
+
 	export let content: ArrayBuffer;
 
 	let src;
 
-	$: {
-		const blob = new Blob([
-			new Uint8Array(content)
-		], {type: "image/jpeg"});
+	const knownHeaders = {
+		jpg: [0xFF, 0xD8],
+		png: [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A],
+	};
 
-		src = window.URL.createObjectURL(blob);
+	const matchesHeader = (bytes, header) => {
+		return header.every((headerByte, i) => headerByte === bytes[i]);
+	};
+
+	$: {
+		const bytes = new Uint8Array(content);
+		let blob;
+
+		if (matchesHeader(bytes, knownHeaders.jpg)) {
+			blob = new Blob([bytes], {type: "image/jpeg"});
+			src = window.URL.createObjectURL(blob);
+		} else if (matchesHeader(bytes, knownHeaders.png)) {
+			blob = new Blob([bytes], {type: "image/png"});
+			src = window.URL.createObjectURL(blob);
+		} else {
+			const tga = tgaImage.imageWithData(content);
+			tga.didLoad.then(() => {
+				src = tga.image.src;
+			});
+		}
 	}
 </script>
 
@@ -23,6 +44,6 @@
 		text-align: center;
 	}
 	img {
-		background-color: white;
+		background: url('images/tile.png');
 	}
 </style>
