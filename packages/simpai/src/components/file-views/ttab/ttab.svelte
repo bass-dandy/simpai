e96b4b337fa8d0592999e43e47a67cfa39316fae
bass-dandy <ctdinh93@gmail.com>
2023-04-21 03:45:39 -0400

    implement add/remove for TTAB items

--- packages/simpai/src/components/file-views/ttab/ttab.svelte
+++ packages/simpai/src/components/file-views/ttab/ttab.svelte
@@ -3,10 +3,14 @@
 	import produce from 'immer';
 
 	import Box from '$components/shared/box.svelte';
+	import Button from '$components/shared/button.svelte';
+	import CloseButton from '$components/shared/close-button.svelte';
 	import TabPanel from '$components/shared/tab-panel.svelte';
 	import TextInput from '$components/shared/text-input.svelte';
+	import { defaultFileData } from '$lib/consts';
 	import { select } from '$lib/selectors';
 	import { packages } from '$lib/stores';
+	import { without } from '$lib/util';
 
 	import MotiveTables from './motive-tables.svelte';
 	import Settings from './settings.svelte';
@@ -27,6 +31,20 @@
 		).map((item) => item.value);
 	};
 
+	const remove = (i: number) => onChange(
+		produce(content, (draft) => {
+			draft.items = without(draft.items, i);
+		})
+	);
+
+	const append = () => onChange(
+		produce(content, (draft) => {
+			draft.items.push({
+				...(defaultFileData.TTAB.items[0] as TtabContent['items'][number])
+			});
+		})
+	);
+
 	const handleFormatChange = (val: string | number) => onChange(
 		produce(content, (draft) => {
 			draft.format = val as number;
@@ -36,62 +54,75 @@
 
 <div class="ttab-view">
 	<Box secondary>
-		<div class="controls">
-			<label>
-				Interaction
-				<select
-					value={index}
-					on:change={(e) => {
-						index = parseInt(e.currentTarget.value);
-					}}
-					class="index-select"
-				>
-					{#each content.items as item, i}
-						<option value={i}>
+		<div class="controls-x">
+			<ol class="interaction-select" role="listbox">
+				{#each content.items as item, i}
+					<li class:active-interaction={index === i}>
+						<CloseButton
+							onClick={() => remove(i)}
+							aria-label={`remove interaction ${i}`}
+						/>
+						<button
+							class="interaction-option"
+							on:click={() => { index = i; }}
+							role="option"
+							aria-selected={index === i}
+						>
 							({i}) {ttasItems?.[item.strIndex] ?? ''}
-						</option>
-					{/each}
-				</select>
-			</label>
-			<TextInput
-				label="Format"
-				variant="hex"
-				maxLength={8}
-				onChange={handleFormatChange}
-				value={content.format}
-				style="width: 100%"
-			/>
+						</button>
+					</li>
+				{/each}
+			</ol>
+			<div class="controls-y">
+				<TextInput
+					label="Format"
+					variant="hex"
+					maxLength={8}
+					onChange={handleFormatChange}
+					value={content.format}
+					style="width: 100%"
+				/>
+				<Button onClick={append} style="margin-bottom: 5px">
+					Append new interaction
+				</Button>
+			</div>
 		</div>
 	</Box>
-	<TabPanel
-		tabs={{
-			settings: {
-				title: 'Settings',
-				content: Settings,
-				hideClose: true,
-				props: { content, onChange, index },
-			},
-			motives: {
-				title: 'Motive tables',
-				content: MotiveTables,
-				hideClose: true,
-				props: { content, onChange, index },
-			},
-		}}
-		activeTab={activeTab}
-		onChange={(id) => activeTab = id}
-		onClose={() => null}
-		headingLevel="h3"
-		style={{
-			flex: '1',
-			'background': 'transparent',
-			border: '0',
-			overflow: 'hidden',
-			padding: '0',
-			'margin-top': '5px',
-		}}
-		contentStyle={{ overflow: 'auto' }}
-	/>
+	{#if content.items[index]}
+		<TabPanel
+			tabs={{
+				settings: {
+					title: 'Settings',
+					content: Settings,
+					hideClose: true,
+					props: { content, onChange, index },
+				},
+				motives: {
+					title: 'Motive tables',
+					content: MotiveTables,
+					hideClose: true,
+					props: { content, onChange, index },
+				},
+			}}
+			activeTab={activeTab}
+			onChange={(id) => activeTab = id}
+			onClose={() => null}
+			headingLevel="h3"
+			style={{
+				flex: '1',
+				'background': 'transparent',
+				border: '0',
+				overflow: 'hidden',
+				padding: '0',
+				'margin-top': '5px',
+			}}
+			contentStyle={{ overflow: 'auto' }}
+		/>
+	{:else}
+		<Box secondary style={{ 'margin-top': 'var(--spacing-sm)' }}>
+			No interaction selected
+		</Box>
+	{/if}
 </div>
 
 <style>
@@ -100,15 +131,47 @@
 		flex-direction: column;
 		height: 100%;
 	}
-	.controls {
-		display: grid;
-		grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
-		gap: 15px;
-		min-width: 0;
-		overflow: hidden;
+	.controls-x {
+		display: flex;
+		gap: var(--spacing-md);
 	}
-	.index-select {
-		display: block;
+	.controls-y {
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-md);
+		white-space: nowrap;
+	}
+	.interaction-select {
 		width: 100%;
+		height: 100px;
+		overflow: auto;
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		border: 1px solid var(--color-accent);
+	}
+	.interaction-select li {
+		display: flex;
+		align-items: center;
+		gap: var(--spacing-sm);
+		padding: 0 var(--spacing-sm);
+		background-color: var(--color-input);
+	}
+	.interaction-select li:nth-child(odd) {
+		background-color: var(--color-btn);
+	}
+	.interaction-select li.active-interaction {
+		background-color: var(--color-bg);
+	}
+	.interaction-option {
+		display: block;
+		flex: 1;
+		padding: var(--spacing-sm);
+		border: 0;
+		background-color: transparent;
+		text-align: left;
+	}
+	.interaction-select li.active-interaction .interaction-option {
+		color: var(--color-input);
 	}
 </style>
