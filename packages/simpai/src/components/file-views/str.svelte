7df0f0abb642db8775caf8f2649fc3c033d0097e
bass-dandy <ctdinh93@gmail.com>
2023-04-21 01:25:28 -0400

    implement add/remove for STR view

--- packages/simpai/src/components/file-views/str.svelte
+++ packages/simpai/src/components/file-views/str.svelte
@@ -3,17 +3,20 @@
 	import produce from 'immer';
 
 	import Box from '$components/shared/box.svelte';
+	import Button from '$components/shared/button.svelte';
+	import CloseButton from '$components/shared/close-button.svelte';
 
-	import {getLanguage} from '$lib/util';
-	import {languages} from '$lib/consts';
+	import { formatHex, getLanguage, without } from '$lib/util';
+	import { languages, defaultFileData } from '$lib/consts';
 
 	export let content: StrContent;
 	export let onChange: (newContent: StrContent) => void;
 
-	const handleDescChange = (e: Event, i: number) => onChange(
+	const append = () => onChange(
 		produce(content, (draft) => {
-			const tgt = draft.stringSets[i];
-			if (tgt) tgt.description = (e.target as HTMLInputElement).value;
+			draft.stringSets.push({
+				...(defaultFileData.STR.stringSets[0] as StrContent['stringSets'][number])
+			});
 		})
 	);
 
@@ -30,79 +33,120 @@
 			if (tgt) tgt.value = (e.target as HTMLTextAreaElement).value;
 		})
 	);
+
+	const handleDescChange = (e: Event, i: number) => onChange(
+		produce(content, (draft) => {
+			const tgt = draft.stringSets[i];
+			if (tgt) tgt.description = (e.target as HTMLTextAreaElement).value;
+		})
+	);
 </script>
 
-<ul>
-{#each content.stringSets as stringSet, i (i)}
-	<Box as="li" style={{ 'margin-top': i ? '5px' : '0' }} secondary>
-		<div class="meta">
-			<div class="description">
-				<label>
-					Description
-					<input
-						type="text"
-						placeholder="No description"
-						value={stringSet.description}
-						on:input={(e) => handleDescChange(e, i)}
-					/>
-				</label>
-			</div>
-			<div class="language">
-				<label>
-					Language
-					<select
-						class="language"
-						value={stringSet.languageId}
-						on:input={(e) => handleLangChange(e, i)}
-					>
-					{#each languages as _, i}
-						<option value={i + 1}>{getLanguage(i + 1)}</option>
-					{/each}
-					</select>
-				</label>
-			</div>
-		</div>
-		<label>
-			Value
-			<textarea
-				class="value"
-				placeholder="No value"
-				value={stringSet.value}
-				on:input={(e) => handleValChange(e, i)}
-			/>
-		</label>
+<div class="str-view">
+	<Box secondary>
+		<Button onClick={append}>
+			Append new string set
+		</Button>
 	</Box>
-{/each}
-</ul>
+	<Box secondary style={{ flex: '1', overflow: 'auto', 'padding-top': '0' }}>
+		<table>
+			<thead>
+				<th class="shrink" />
+				<th>Value</th>
+				<th>Description</th>
+			</thead>
+			<tbody>
+				{#each content.stringSets as stringSet, i (i)}
+					<tr>
+						<td>
+							<div class="grid">
+								<CloseButton
+									onClick={() => onChange(
+										produce(content, (draft) => {
+											draft.stringSets = without(draft.stringSets, i);
+										})
+									)}
+									aria-label={`remove string set ${i}`}
+								/>
+								({formatHex(i, 4)})
+								<select
+									class="language"
+									value={stringSet.languageId}
+									on:input={(e) => handleLangChange(e, i)}
+								>
+									{#each languages as _, i}
+										<option value={i + 1}>{getLanguage(i + 1)}</option>
+									{/each}
+								</select>
+							</div>
+						</td>
+						<td>
+							<textarea
+								placeholder="No value"
+								value={stringSet.value}
+								on:input={(e) => handleValChange(e, i)}
+							/>
+						</td>
+						<td>
+							<textarea
+								placeholder="No description"
+								value={stringSet.description}
+								on:input={(e) => handleDescChange(e, i)}
+							/>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</Box>
+</div>
 
 <style>
-	ul {
-		list-style: none;
-		margin: 0;
-		padding: 0;
-		height: 100%;
-		overflow: auto;
-	}
-	label {
-		display: block;
-		margin-bottom: 0;
-	}
-	.meta {
+	.str-view {
 		display: flex;
-		margin-bottom: 15px;
+		flex-direction: column;
+		height: 100%;
+		gap: var(--spacing-sm);
 	}
-	.description {
-		flex: 1;
-		margin-right: 15px;
-	}
-	input {
+	table {
+		border-spacing: 0;
+		position: relative;
 		width: 100%;
 	}
-	.language {
-		display: block;
+	th, td {
+		padding: var(--spacing-sm);
+		text-align: left;
+		font-size: 1rem;
+		border-bottom: 1px solid var(--color-accent);
+		white-space: nowrap;
+		vertical-align: top;
 	}
-	.value {
-		resize: vertical;
+	tr:last-child td {
+		border-bottom: 0;
+	}
+	th {
+		position: sticky;
+		top: 0;
+		padding-top: var(--spacing-md);
+		background-color: var(--color-fg);
+	}
+	.shrink {
+		width: 0;
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		align-items: flex-start;
+		gap: var(--spacing-sm);
+	}
+	.grid select {
+		grid-column: span 2;
+		min-width: 125px;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	textarea {
+		min-height: 51px;
 		width: 100%;
 	}
 </style>
